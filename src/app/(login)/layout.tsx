8fabdeddcf59c7776fefe3f50e790aefc0f56adb
Setthawut Kasiwutchoudchuchai <79016020+big7983@users.noreply.github.com>
2025-01-16 17:07:47 +0700

    Update layout.tsx

---
+++ src/app/(login)/layout.tsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/app/globals.css";
+import Navber from "@/components/Navber";
+import Navberdebug from "@/components/Navberdebug";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
