cd36c48b833daa340ce7a07cc8f31e2c062eb82e
Setthawut Kasiwutchoudchuchai <79016020+big7983@users.noreply.github.com>
2025-01-30 15:47:54 +0700

    V0.1.1
    - แก้ eslint

--- src/app/(debug)/realtime/page.tsx
+++ src/app/(debug)/realtime/page.tsx
@@ -1,35 +1,41 @@
-"use client"
+// "use client"
 
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
 
-export default function RealtimeData() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState("");
+// export default function RealtimeData() {
+//   const [data, setData] = useState(null);
+//   const [error, setError] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://678909dd2c874e66b7d75b1b.mockapi.io/users');
-        setData(response.data);
-      } catch (error) {
-        setError("error");
-      }
-    };
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('https://678909dd2c874e66b7d75b1b.mockapi.io/users');
+//         setData(response.data);
+//       } catch () {
+//       }
+//     };
 
-    fetchData();
-    const interval = setInterval(fetchData, 3000); // ดึงข้อมูลใหม่ทุกๆ 3 วินาที
+//     fetchData();
+//     const interval = setInterval(fetchData, 3000); // ดึงข้อมูลใหม่ทุกๆ 3 วินาที
 
-    return () => clearInterval(interval); // ล้าง interval เมื่อ component ถูก unmount
-  }, []);
+//     return () => clearInterval(interval); // ล้าง interval เมื่อ component ถูก unmount
+//   }, []);
 
-  if (error) return <p>เกิดข้อผิดพลาด: {error}</p>;
-  if (!data) return <p>กำลังโหลดข้อมูล...</p>;
+//   if (error) return <p>เกิดข้อผิดพลาด: {error}</p>;
+//   if (!data) return <p>กำลังโหลดข้อมูล...</p>;
 
+//   return (
+//     <div>
+//       <h1>ข้อมูลแบบเรียลไทม์</h1>
+//       <pre>{JSON.stringify(data, null, 2)}</pre>
+//     </div>
+//   );
+// }
+import React from 'react'
+
+export default function page() {
   return (
-    <div>
-      <h1>ข้อมูลแบบเรียลไทม์</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+    <div>page</div>
+  )
 }
